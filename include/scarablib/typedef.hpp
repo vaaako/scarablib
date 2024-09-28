239e5f57ee71bf12b83c81479537f0226aceac4b
vaaako <vakofernanda@gmail.com>
2024-09-28 12:43:46 -0300

    new types for typedef

---
+++ include/scarablib/typedef.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+// int
+typedef int8_t int8;   // signed char
+typedef int16_t int16; // short
+typedef int32_t int32; // int
+typedef int64_t int64; // long
+
+// unsigned
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+// float
+typedef double float64;
