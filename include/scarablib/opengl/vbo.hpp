fd68077a2a47c4e2e69c85155509ff83b6ff15b7
vaaako <vakofernanda@gmail.com>
2024-10-01 15:52:44 -0300

    chore: changed description of link_attrib

--- include/scarablib/opengl/vbo.hpp
+++ include/scarablib/opengl/vbo.hpp
@@ -25,14 +25,15 @@ class VBO {
 		// Allocate and initialize the data store for the VBO.
 		// `size` is the total size of the data (in bytes) to allocate, and `data` is a pointer to the data.
 		// If `data` is nullptr, the buffer is allocated but left uninitialized.
-		void alloc_data(const uint64 size, const void* data);
+		void alloc_data(const uint64 size, const void* data, const GLenum drawtype = GL_STATIC_DRAW);
 
 		// Link a vertex attribute to the VBO for use in a shader.
-		// `index` is the index of the vertex attribute in the shader.
-		// `dimension` is the number of components per vertex attribute (e.g., 3 for vec3).
+		// `index` is the index of the vertex attribute in vertex shader. (e.g, "`layout (location = index) in ...`")
+		// `dimension` is the number of components per vertex attribute (e.g., 3 for vec3, 2 for vec2).
 		// `total_byte_size` is the total size of a single vertex (e.g., sum of all dimensions times `sizeof(flaot)`).
-		// `offset` is the byte offset of the attribute in a vertex (e.g., texture offset is 2 in a 2D vertex, because position has 2 dimensions and texture comes after it).
+		// `offset` is the byte offset of the attribute in a vertex (e.g., texture offset of a 3D mesh is is `3 * sizeof(float)`, since position has 3 float values and texture comes after it)
 		void link_attrib(const uint32 index, const uint32 dimension, const int total_byte_size, const uint32 offset);
+
 	private:
 		GLuint id;
 };
