e7505f3b4def490ec06f769f059ac2663b774eaf
vako <vakofernanda@gmail.com>
2025-02-20 20:12:45 -0300

    style: name fix

--- include/scarablib/gfx/3d/cube.hpp
+++ include/scarablib/gfx/3d/cube.hpp
@@ -3,8 +3,8 @@
 #include "scarablib/gfx/model.hpp"
 
 // This is a class to make a cube model
-// WARNING: Do not use this class directly, use ModelFactory::create_cube(const ModelConf& conf)
+// WARNING: Do not use this class directly, use ModelFactory::create_cube(const Model::Config& conf)
 struct Cube : public Model {
-	// WARNING: Do not use this class directly, use ModelFactory::create_cube(const ModelConf& conf)
+	// WARNING: Do not use this class directly, use ModelFactory::create_cube(const Model::Config& conf)
 	Cube(const Model::Config& conf, const std::vector<Vertex>& vertices, const std::vector<uint32>& indices) noexcept;
 };
--- include/scarablib/gfx/3d/plane.hpp
+++ include/scarablib/gfx/3d/plane.hpp
@@ -4,8 +4,8 @@
 #include <vector>
 
 // This is a class to make a plane model.
-// WARNING: Do not use this class directly, use ModelFactory::create_cube(const ModelConf& conf)
+// WARNING: Do not use this class directly, use ModelFactory::create_cube(const Model::Config& conf)
 struct Plane : public Model {
-	// WARNING: Do not use this class directly, use ModelFactory::create_cube(const ModelConf& conf)
+	// WARNING: Do not use this class directly, use ModelFactory::create_cube(const Model::Config& conf)
 	Plane(const Model::Config& conf, const std::vector<Vertex>& vertices, const std::vector<uint32>& indices) noexcept;
 };
