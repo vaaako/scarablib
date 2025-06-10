4d82a808868389cd7d0532304654fb6e608b5c08
vako <vakofernanda@gmail.com>
2025-06-10 14:28:01 -0300

    updated extension check

--- src/types/Iaudio.cpp
+++ src/types/Iaudio.cpp
@@ -3,14 +3,23 @@
 #include "scarablib/proper/error.hpp"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
+#include <array>
 
 IAudio::IAudio(const char* path) {
-	// Check if extension is supported
+	// Supported extensions lookup table
+	constexpr std::array<const std::string_view, 5> supported_extensions = {
+		"wav", "ogg", "flac", "mp3", "opus"
+	};
+
 	const std::string extension = StringHelper::file_extension(path);
-	if(!(extension == "wav" || extension == "ogg" || extension == "flac" || extension == "mp3")) {
+
+	if(std::ranges::find(supported_extensions.begin(), supported_extensions.end(), extension) == supported_extensions.end()) {
 		SDL_Quit();
 		Mix_CloseAudio();
-		throw ScarabError("Audio format is not supported (%s)\nSupported formats are: WAV, MP3, OGG and FLAC", path);
+		throw ScarabError(
+			"Audio format is not supported (%s)\nSupported formats are: WAV, MP3, OGG, OPUS and FLAC", 
+			path
+		);
 	}
 }
 
