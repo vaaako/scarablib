7a858bc7512b3a5b9498d87c6ca88b63659dd05a
vako <vakofernanda@gmail.com>
2025-06-30 18:46:15 -0300

    added EBO to skybox

--- src/gfx/3d/skybox.cpp
+++ src/gfx/3d/skybox.cpp
@@ -1,4 +1,5 @@
 #include "scarablib/gfx/3d/skybox.hpp"
+#include "scarablib/gfx/texture.hpp"
 #include "scarablib/proper/error.hpp"
 #include "scarablib/typedef.hpp"
 #include "scarablib/gfx/image.hpp"
@@ -6,68 +7,56 @@
 Skybox::Skybox(const Camera& camera, const std::array<const char*, 6>& faces)
 	: camera(camera) {
 
-	// All cube faces
-	// NOTE: Dont make static since a skybox will created only one or two times, so its a waste of memory
-	std::vector<vec3<float>> vertices = {
-		{ -1.0f,  1.0f, -1.0f },
-		{ -1.0f, -1.0f, -1.0f },
-		{  1.0f, -1.0f, -1.0f },
-		{  1.0f, -1.0f, -1.0f },
-		{  1.0f,  1.0f, -1.0f },
-		{ -1.0f,  1.0f, -1.0f },
-
-		{ -1.0f, -1.0f,  1.0f },
-		{ -1.0f, -1.0f, -1.0f },
-		{ -1.0f,  1.0f, -1.0f },
-		{ -1.0f,  1.0f, -1.0f },
-		{ -1.0f,  1.0f,  1.0f },
-		{ -1.0f, -1.0f,  1.0f },
-
-		{  1.0f, -1.0f, -1.0f },
-		{  1.0f, -1.0f,  1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{  1.0f,  1.0f, -1.0f },
-		{  1.0f, -1.0f, -1.0f },
-
-		{ -1.0f, -1.0f,  1.0f },
-		{ -1.0f,  1.0f,  1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{  1.0f, -1.0f,  1.0f },
-		{ -1.0f, -1.0f,  1.0f },
-
-		{ -1.0f,  1.0f, -1.0f },
-		{  1.0f,  1.0f, -1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{  1.0f,  1.0f,  1.0f },
-		{ -1.0f,  1.0f,  1.0f },
-		{ -1.0f,  1.0f, -1.0f },
-
-		{ -1.0f, -1.0f, -1.0f },
-		{ -1.0f, -1.0f,  1.0f },
-		{  1.0f, -1.0f, -1.0f },
-		{  1.0f, -1.0f, -1.0f },
-		{ -1.0f, -1.0f,  1.0f },
-		{  1.0f, -1.0f,  1.0f }
+	static const std::vector<vec3<float>> vertices = {
+		{ -1.0f,  1.0f, -1.0f }, // 0 - Top-Left-Back
+		{  1.0f,  1.0f, -1.0f }, // 1 - Top-Right-Back
+		{  1.0f, -1.0f, -1.0f }, // 2 - Bottom-Right-Back
+		{ -1.0f, -1.0f, -1.0f }, // 3 - Bottom-Left-Back
+		{ -1.0f,  1.0f,  1.0f }, // 4 - Top-Left-Front
+		{  1.0f,  1.0f,  1.0f }, // 5 - Top-Right-Front
+		{  1.0f, -1.0f,  1.0f }, // 6 - Bottom-Right-Front
+		{ -1.0f, -1.0f,  1.0f }  // 7 - Bottom-Left-Front
 	};
 
-	this->bundle.make_vao(vertices, std::vector<uint8> {});
+	static const std::vector<uint32> indices = {
+		// Back face
+		0, 3, 2,
+		2, 1, 0,
+		// Front face
+		4, 5, 6,
+		6, 7, 4,
+		// Left face
+		7, 3, 0,
+		0, 4, 7,
+		// Right face
+		1, 2, 6,
+		6, 5, 1,
+		// Top face
+		0, 1, 5,
+		5, 4, 0,
+		// Bottom face
+		3, 7, 6,
+		6, 2, 3
+	};
+
+	this->bundle.make_vao(vertices, indices);
 
 	// Gen texture cube map
 	glGenTextures(1, &this->texid);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, this->texid);
 
 	for(uint16 i = 0; i < 6; i++) {
-		// why when i set this to false the skybox bugs? (true actually makes the textures NOT flip on load, see image.cpp)
+		// OpenGL loads cubemap textures differently, so here makes sense enabling flip vertically (which actually disables)
 		Image* image = new Image(faces.at(i), false, true);
 		if(image->data == nullptr) {
+			delete image;
 			throw ScarabError("Image (%s) in skybox was not found", image->path);
 		}
 
 		// Order: Right > Left > Top > Bottom > Back > Front
+		const GLenum format = Texture::extract_format(*image, false);
 		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-				0, GL_RGB, image->width, image->height, 0, GL_RGB, GL_UNSIGNED_BYTE, image->data);
+				0, static_cast<GLint>(format), image->width, image->height, 0, format, GL_UNSIGNED_BYTE, image->data);
 
 		delete image;
 	}
@@ -89,18 +78,18 @@ void Skybox::draw() noexcept {
 	// glDepthFunc(GL_LEQUAL);
 
 	this->shader->use();
-	glBindTexture(GL_TEXTURE_CUBE_MAP, this->texid);
-
 	this->shader->set_matrix4f("view", glm::mat3(camera.get_view_matrix())); // Conversion: Remove translation
 	this->shader->set_matrix4f("proj", camera.get_proj_matrix());
 
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, this->texid);
+
 	this->bundle.vao->bind();
-	glDrawArrays(GL_TRIANGLES, 0, 36);
-	this->bundle.vao->bind();
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	this->bundle.vao->unbind();
 
 	this->shader->unbind();
 	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
 
-	// Back to default
 	// glDepthFunc(GL_LESS);
 }
