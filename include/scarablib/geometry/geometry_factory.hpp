280c689b26f5bb3c1a1d387c85250f4be2f4ac4e
vako <vakofernanda@gmail.com>
2025-07-07 12:13:53 -0300

    moved cube face construction to GeometryFactory

--- include/scarablib/geometry/geometry_factory.hpp
+++ include/scarablib/geometry/geometry_factory.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "scarablib/geometry/vertex.hpp"
+#include <utility>
 
 // Functions that returns the vertices for different geometries
 namespace GeometryFactory {
 	// Returns the vertices for all faces of a cube
 	std::vector<Vertex> make_cube_vertices() noexcept;
+	// Returns a pair containing the vertices and indices from a cube
+	std::pair<std::vector<Vertex>, std::vector<uint8>> make_cube_faces(const uint8 face_mask) noexcept;
 	// Returns the vertices for a plane
 	std::vector<Vertex> make_plane_vertices() noexcept;
 
--- src/gfx/3d/cube.cpp
+++ src/gfx/3d/cube.cpp
@@ -4,42 +4,6 @@
 Cube::Cube(const uint8 face_mask) noexcept
 	: Model() {
 
-	// Source vertices
-	const std::vector<Vertex> vertices = GeometryFactory::make_cube_vertices();
-
-	// New vertices
-	std::vector<Vertex> final_vertices;
-	std::vector<uint8> indices;
-
-	// Indices for one face
-	constexpr uint8 indices_index[6] = { 0, 1, 2, 0, 2, 3 };
-
-	// Offset of one mesh to another
-	uint32 vertex_offset = 0;
-
-	// Each face adds 6 vertices, 6 indices
-	for(uint32 v = 0; v < 6; v++) {
-		// Hide face if included
-		if((face_mask & (1 << v)) == 0) {
-			continue;
-		}
-
-		// Where this face's vertices are in cube vertices
-		const uint32 start_index = v * 4; // Vertices per face
-
-		final_vertices.insert(
-			final_vertices.end(), // Insert at the end
-			vertices.begin() + start_index, // Start copying from here
-			vertices.begin() + start_index + 4 // Stop copying here
-		);
-
-		// Build indices
-		for(int i = 0; i < 6; i++) {
-			indices.push_back(static_cast<uint8>(indices_index[i] + vertex_offset));
-		}
-		// Amount of vertices to skip (next face)
-		vertex_offset += 4;
-	}
-
-	this->set_geometry(final_vertices, indices);
+	auto geometry = GeometryFactory::make_cube_faces(face_mask);
+	this->set_geometry(geometry.first, geometry.second);
 }
