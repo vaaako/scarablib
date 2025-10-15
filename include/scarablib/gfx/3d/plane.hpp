9c14eee8356c58027d7be558fb53ba78e7aceba3
vako <vakofernanda@gmail.com>
2025-10-15 17:38:54 -0300

    moved comments

--- include/scarablib/gfx/3d/plane.hpp
+++ include/scarablib/gfx/3d/plane.hpp
@@ -4,15 +4,13 @@
 
 // Plane model object
 struct Plane : public Model {
-	// The type of the plane
-	// - SINGLE_PLANE: Only one plane
-	// - CROSSED_PLANE: Two crossed planes in X shape
-	// - FOUR_CROSSED_PLANE: Four crossed planes in X shape. Where two planes are opposite to each other
-	//
-	// FOUR_CROSSED_PLANE has a better effect but only works with CULLING_FACE enabled
 	enum Type : uint8 {
+		// Only one plane
 		SINGLE_PLANE,
+		// Two crossed planes in X shape
 		CROSSED_PLANE,
+		// Four crossed planes in X shape.
+		// Where two planes are opposite to each other
 		FOUR_CROSSED_PLANE
 	};
 
